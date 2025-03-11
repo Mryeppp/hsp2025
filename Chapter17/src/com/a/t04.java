b4e0ba83e2254fe94b5b8735106ba6173878f6af
Mryeppp <106108155+Mryeppp@users.noreply.github.com>
2025-03-11 23:01:00 +0800

    t04

--- Chapter17/src/com/a/t04.java
+++ Chapter17/src/com/a/t04.java
@@ -1,8 +1,21 @@
 package com.a;
-
-public class t04 {
+public class t04{
   public static void main(String[] args) {
-    System.out.println();
+    
   }
 }
-
+calss SaleWindows extends Thread {
+  @Override 
+  int tickets = 1000 ;
+  public void run() {
+    whlile (true) {
+      if(tickets <= 0) { break; }
+      try {
+        Thread.sleep(1000)
+      } catch (InterruptException e) {
+        e.printStackTrace();
+      }
+      System.out.println(Thread.currentThread().getName() + "买了一张票，剩余票数 " + --tickets);
+    }
+  }
+}
