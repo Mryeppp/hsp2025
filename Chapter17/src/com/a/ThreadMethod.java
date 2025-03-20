737230c47165dcb165008749e81666fa8cc7d171
Mryeppp <106108155+Mryeppp@users.noreply.github.com>
2025-03-20 22:54:03 +0800

    new

--- Chapter17/src/com/a/ThreadMethod.java
+++ Chapter17/src/com/a/ThreadMethod.java
@@ -2,6 +2,7 @@ package com.a;
 
 public class ThreadMethod {
     public static void main(String[] args) {
-       System.out.println("a");
-    }
+      System.out.println("a");
+      System.out.println("b");
+  }
 }
